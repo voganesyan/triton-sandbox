2c20ce7372607c1251c2e0117f794f6f4d57703d
Vitaliy Oganesyan <vitaly.oganesyan@iss.ru>
2024-03-19 22:26:13 +0300

    Add OCSort tracker

--- yolov9/client.py
+++ yolov9/client.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import tritonclient.grpc as grpcclient
+from ocsort.ocsort import OCSort
 
 def draw_detections(img, detections: list, color = (0, 255, 0)):
     for detection in detections:
@@ -13,6 +14,18 @@ def draw_detections(img, detections: list, color = (0, 255, 0)):
 
 
 client = grpcclient.InferenceServerClient(url='localhost:8001')
+
+tracker = OCSort(
+    per_class=True,
+    det_thresh=0,
+    max_age=30,
+    min_hits=1,
+    asso_threshold=0.3,
+    delta_t=3,
+    asso_func="giou",
+    inertia=0.2,
+    use_byte=False,
+)
 
 cap = cv2.VideoCapture('test_data/MOT17-04-SDP-raw.webm')
 if not cap.isOpened():
@@ -32,6 +45,7 @@ while True:
     results = client.infer(model_name='ensemble_model', inputs=[input_tensor])
     detections = results.as_numpy('detections')
     detections = np.squeeze(detections, axis=0)
+    res = tracker.update(detections, frame)
     draw_detections(frame, detections)
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) == ord('q'):
