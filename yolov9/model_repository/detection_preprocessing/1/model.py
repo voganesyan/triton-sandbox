c6789d8c54be4df3556020219361e696e3569dde
Vitaliy Oganesyan <vitaly.oganesyan@iss.ru>
2024-03-19 22:27:04 +0300

    Remove comment

--- yolov9/model_repository/detection_preprocessing/1/model.py
+++ yolov9/model_repository/detection_preprocessing/1/model.py
@@ -1,11 +1,6 @@
 import json
 import numpy as np
 import cv2
-
-# triton_python_backend_utils is available in every Triton Python model. You
-# need to use this module to create inference requests and responses. It also
-# contains some utility functions for extracting information from model_config
-# and converting Triton input/output types to numpy types.
 import triton_python_backend_utils as pb_utils
 
 
