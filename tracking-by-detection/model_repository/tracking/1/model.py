f6d3467244809f4dc145b4b03a3baa795f7608bf
Vitaliy Oganesyan <vitaly.oganesyan@iss.ru>
2024-04-01 23:44:39 +0300

    Add support of multiple trackers

--- tracking-by-detection/model_repository/tracking/1/model.py
+++ tracking-by-detection/model_repository/tracking/1/model.py
@@ -14,31 +14,41 @@ class TritonPythonModel:
         self.tracks_dtype = pb_utils.triton_string_to_numpy(
             tracks['data_type']
         )
-        self.tracker = OCSort(
-            per_class=True,
-            det_thresh=0,
-            max_age=30,
-            min_hits=1,
-            asso_threshold=0.3,
-            delta_t=3,
-            asso_func='giou',
-            inertia=0.2,
-            use_byte=False,
-        )
+        self.trackers = {}
 
     def execute(self, requests):
         responses = []
         for request in requests:
-            detections = pb_utils.get_input_tensor_by_name(
-                request, 'detections'
-            )
-            frame = pb_utils.get_input_tensor_by_name(
-                request, 'frame'
-            )
+            seq_start = pb_utils.get_input_tensor_by_name(request, "START")
+            seq_start = seq_start.as_numpy()[0][0]
+            seq_id = pb_utils.get_input_tensor_by_name(request, "CORRID")
+            seq_id = seq_id.as_numpy()[0][0]
+            seq_end = pb_utils.get_input_tensor_by_name(request, "END")
+            seq_end = seq_end.as_numpy()[0][0]
+
+            if seq_start:
+                print('SS ', seq_id, flush=True)
+                self.trackers[seq_id] = OCSort(
+                    per_class=True,
+                    det_thresh=0,
+                    max_age=30,
+                    min_hits=1,
+                    asso_threshold=0.3,
+                    delta_t=3,
+                    asso_func='giou',
+                    inertia=0.2,
+                    use_byte=False,
+                )
+            if seq_end:
+                print('SE ', seq_id, flush=True)
+                self.trackers.pop(seq_id)
+
+            detections = pb_utils.get_input_tensor_by_name(request, 'detections')
+            frame = pb_utils.get_input_tensor_by_name(request, 'frame')
             detections = np.squeeze(detections.as_numpy(), axis=0)
             frame = np.squeeze(frame.as_numpy(), axis=0)
 
-            tracks = self.tracker.update(detections, frame)
+            tracks = self.trackers[seq_id].update(detections, frame)
             tracks = np.expand_dims(tracks, axis=0)
             tracks = pb_utils.Tensor(
                 'tracks', tracks.astype(self.tracks_dtype)
